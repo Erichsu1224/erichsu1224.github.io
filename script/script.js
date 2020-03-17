06444ec8df06c3fb2688e2b6bb1ad75d9489752e
Erichsu1224 <40513062+Erichsu1224@users.noreply.github.com>
2020-03-17 12:44:40 +0800

    new personal file

---
+++ script/script.js
@@ -0,0 +1,11 @@
+
+
+$(document).on('click','a',function(event){
+    //event.preventDefault();
+    var target= $(this).attr("href");
+    $('html,body').animate({
+      scrollTop: $(target).offset().top
+    },500);
+    
+});
+  
